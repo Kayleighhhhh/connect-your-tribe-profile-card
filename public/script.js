180bc8f8f99d3458754b9ac71e7faa2f2615a24c
Kayleighhhhh <kayleigh.vdham04@gmail.com>
2026-02-04 12:45:15 +0100

    #10 #5

--- public/script.js
+++ public/script.js
@@ -3,24 +3,26 @@ const hour = document.getElementById("hour");
 const minute = document.getElementById("minute");
 const dateText = document.getElementById("date");
 const lock = document.getElementById("lock");
+const home = document.getElementById("home");
 const profile = document.getElementById("profile");
 const screen = document.getElementById("screen");
+const aboutMe = document.getElementById("aboutMe");
 
 // UPDATE TIME
 function updateTime() {
-  const now = new Date();
+    const now = new Date();
 
-  let hours = now.getHours();
-  let minutes = now.getMinutes();
+    let hours = now.getHours();
+    let minutes = now.getMinutes();
 
-  if (hours < 10) hours = "0" + hours;
-  if (minutes < 10) minutes = "0" + minutes;
+    if (hours < 10) hours = "0" + hours;
+    if (minutes < 10) minutes = "0" + minutes;
 
-  hour.textContent = hours;
-  minute.textContent = minutes;
+    hour.textContent = hours;
+    minute.textContent = minutes;
 
-  const options = { weekday: "short", day: "numeric", month: "long" };
-  dateText.textContent = now.toLocaleDateString(undefined, options);
+    const options = { weekday: "short", day: "numeric", month: "long" };
+    dateText.textContent = now.toLocaleDateString(undefined, options);
 }
 
 // RUN CLOCK
@@ -29,6 +31,17 @@ setInterval(updateTime, 1000);
 
 // UNLOCK PHONE
 screen.addEventListener("click", () => {
-  lock.classList.add("up");
-  profile.classList.add("show");
+    console.log('screen');
+    
+    lock.classList.add("up");
+    home.classList.add("show");
+}, true);
+
+aboutMe.addEventListener("click", () => {
+    console.log('aboutMe');
+    
+    home.classList.remove("show");
+    home.classList.add("up");
+    profile.classList.add("show");
+
 });
